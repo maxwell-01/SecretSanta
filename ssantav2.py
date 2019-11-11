2d5aa5ac839d41511c8974f85a65d38ac1e500c6
Max <newton.maxwell@gmail.com>
2019-11-11 22:27:29 +0000

    changed name checker
    now has issue when last name is the same as no other names to choose from

--- ssantav2.py
+++ ssantav2.py
@@ -1,20 +1,22 @@
+# if the last name is the same then it breaks
+
 import random
 
 names = ["Max","Matthew","Mark","Luke","John","Mike"]
 
 def list_generator(names):
     old_order = names.copy()
-    num_players = len(names)
+    num_players = len(names) - 1
     # the new order of names
     new_order = []
     for i in range(num_players):
-
-        # ranimly selects a name to assign to the new order
-        random_index = random.randrange(0,num_players - i)
-
+        print(i)
+        # creates a random index to pick an assignment
+        random_index = random.randrange(num_players - i)
+        print(random_index)
         # checks if the new name is the same as the name its going to be assigned to
         while True:
-            if random_index != i:
+            if old_order[random_index] != names[i]:
                 # Adds the name to the new order list
                 new_order.append(old_order[random_index])
                 # deletes the name from the old list so it isnt selected again
@@ -23,7 +25,7 @@ def list_generator(names):
             else:
 
                 # Generates a new random number to try again
-                random_index = random.randrange(0,num_players - i)
+                random_index = random.randrange(num_players - i)
     
     if names[num_players - 1] == new_order[num_players - 1]:
         list_generator(names)
