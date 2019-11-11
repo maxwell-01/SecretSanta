f2389b25284a6b9187c98ef9921eea7c1f3f2757
Max <newton.maxwell@gmail.com>
2019-11-11 18:55:13 +0000

    fliped if/else statement

--- ssanta.py
+++ ssanta.py
@@ -9,57 +9,46 @@ def elf_engine():
     print("Welcome to secret santa selctor!")
     num_players = user_input("How many players?",[],"num")
     print("First enter the names one at a time and press enter:")
-    names = namesinput(num_players)
-    new_order = listgenerator(num_players,names)
+    names = names_input(num_players)
+    new_order = list_generator(num_players,names)
     print(new_order)
 
 
 # Function to capture the player names
-def namesinput(num_players):
+def names_input(num_players):
     names = []
     for i in range(1,num_players+1,1):
         names.append(user_input(f"Please enter name number {i}",[],"name"))
     return names
 
-def randomnumber(s,e):
+def random_number(s,e):
 # put random number generator in here
-    random_number = random.randrange(s,e)
-    return random_nmber
+    return random.randrange(s,e)
 
 # Randomises who gets who
-def listgenerator(num_players, names):
+def list_generator(num_players, names):
     old_order = names
     # the new order of names
     new_order = []
-    for i in range(0,num_players,1):
+    for i in range(num_players):
 
         # ranimly selects a name to assign to the new order
-        random_index = randomnumber(0,num_players - {i})
+        random_index = random_number(0,num_players - i)
 
         # checks if the new name is the same as the name its going to be assigned to
-        samenames = 0
         while True:
-            if random_index == i:
-                # Generates a new random number to try again
-                random_index = randomnumber(0,num_players - {i})
-
-            else:
+            if random_index != i:
+                # Adds the name to the new order list
                 new_order = new_order.append(old_order[random_index])
-                del (old_order[random_index]
+                # deletes the name from the old list so it isnt selected again
+                del (old_order[random_index])
                 break
+            else:
+
+                # Generates a new random number to try again
+                random_index = random_number(0,num_players - i)
     
     return new_order
-        
-
-
-        # old_order_num = 
-        # Need to give each old item an index
-        # then randomise the list for the new items
-        # making sure that each name does actually change
-
-    new_order = new_order.append(old_order[random.randrange(0,num_players + 1,1)]
-
-    return comp_choice
 
 # Prompts for user input and checks its valid
 def user_input(msg,options,in_type):
---
+++ tests.py
@@ -0,0 +1,10 @@
+from ssanta import*
+
+def test_names_input():
+    assert random_number(0,0) == 0
+
+def test_list_generator():
+    assert list_generator(2,["max","raefe"]) == ["raefe","max"]
+
+
+
